e831a67a2bb89cf963e94be6cabe2f88fc71c505
kovemu <kovemusin@gmail.com>
2026-09-22 04:21:10 +0900

    chore: connect Only for Human Supabase publishable client

--- site/supabase-config.js
+++ site/supabase-config.js
@@ -1,2 +1,2 @@
 export const SUPABASE_URL = 'https://owkkmgvufjclzoearuuk.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = '';
+export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_4C7YzZJ8NR9aDWwbR11wUA_pWNl0MRl';
